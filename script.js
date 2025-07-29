f18027fe0d62aa9d6f08cde30879220ffe704e48
Manish Zala <zalamanish187@gmail.com>
2025-07-29 16:10:44 +0530

    29-07-2025

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function todo() {
   let list = document.getElementById("list").value;
   let span = document.createElement("span");
-  span.textContent = list;
+  span.innerHTML = list;
+
+  
 
   let icon = document.createElement("i");
   icon.className = "fa-solid fa-square-xmark";
-  let result = list;
   icon.onclick = function () {
     span.remove();
   };
+  span.addEventListener('click',function(e){
+    span.style.textDecoration = 'line-through';
+  })
   span.appendChild(icon);
   document.getElementById("outputShow").appendChild(span);
   document.getElementById("list").value = " ";
